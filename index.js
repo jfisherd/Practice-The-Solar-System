a0c36ca7356a3de96ece999f2b602b2836a72a56
Josh Fisher <jfisherd@gmail.com>
2025-10-28 14:22:19 -0400

    1-4 complete

--- index.js
+++ index.js
@@ -12,16 +12,18 @@ const solarSystem = [
   { name: "Neptune", ringSystem: true, moons: ["Triton", "Nereid"] }
 ];
 // 1. Print the array of Jupiter's moons to the console (no for loop, just print the entire array).
-console.log(solarSystem.jupiter)
+console.log(solarSystem[4].moons)
 
 // 2. Print the name of Neptune's moon "Nereid" to the console.
-
+console.log(solarSystem[7].moons[1])
 
 // 3. Add a new moon called "Endor" to Venus' moons array.
-
+// solarSystem[1].moons.push("Endor")
+solarSystem[1].moons[0] = "Endor"
+console.log(solarSystem[1].moons)
 
 // 4. Add a Pluto object to the solarSystem array using .push. The object should contain Pluto's name, ringSystem boolean, and moons array (which includes "Charon").
-
+solarSystem.push({name: "Pluto", ringSystem: false, moons: ["Charon"]})
 
 // 5. Add a new key-value pair to the the Earth object: the key should be 'diameter', and the value should be Earth's diameter in miles represented as a string.
 
